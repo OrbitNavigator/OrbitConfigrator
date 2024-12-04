72fc926fd7ff4d4dd6ad5ce83c9ccd9a68702097
Witty-Wizard <agarwalshashank429+github@gmail.com>
2024-12-04 18:52:22 +0530

    Added d3.js for displaying graphs

--- src/ui/pages/Home.jsx
+++ src/ui/pages/Home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import Linegraph from "../components/Linegraph";
 
 function Home() {
   return (
     <div>
       <h1>Home</h1>
       <p>Welcome to the Home page!</p>
+      <Linegraph />
     </div>
   );
 }
